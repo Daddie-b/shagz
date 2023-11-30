72589cdf0089dcb90508c7e7e03e27707c72fe8b
Davylhacs <60092325+dav-oss@users.noreply.github.com>
2023-11-30 20:51:32 +0000

    Frontend Working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { animateScroll } from "react-scroll";
+
+import NavBar from "./components/organs/NavBar"
+import Home from "./components/pages/Home";
+import { useEffect } from "react";
+import Footer from "./components/organs/Footer";
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const directory = useLocation();
+  useEffect(() => {
+    animateScroll.scrollToTop({
+      duration: 0,
+    });
+  }, [directory.pathname]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full bg-white text-gray-950 font-poppins">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
